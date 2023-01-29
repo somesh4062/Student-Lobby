fa997515e0d86eb52ad07217abf0a2311c418104
somesh-scrobits <58914964+somesh4062@users.noreply.github.com>
2023-01-29 22:44:22 +0530

    model created

--- lib/home/controller/searchViewController.dart
+++ lib/home/controller/searchViewController.dart
@@ -1,21 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:student_lobby/home/model/serviceModel.dart';
 
 class SearchViewController extends GetxController {
   TextEditingController searchController = TextEditingController();
 
-  searchService(city) {
+  Future<List<ServiceModel>> searchService(city) async {
     var db = FirebaseFirestore.instance;
+    final snapshot =
+        await db.collection("hostel").where("city", isEqualTo: city).get();
+    final servicedata =
+        snapshot.docs.map((e) => ServiceModel.fromSnapshot(e)).toList();
+    debugPrint(servicedata.toString());
+    return servicedata;
+    // var details;
+    // final cityRef = db.collection("hostel");
 
-    final cityRef = db
-        .collection("hostel")
-        .where("city", isEqualTo: city)
-        .get()
-        .then((value) => {
-          debugPrint(value.toString())
-        });
     // final query = cityRef.where("city", isEqualTo: city).get().then((value) => {
     //       //  debugPrint(city),
     //       debugPrint(value.toString())
---
+++ lib/home/model/serviceModel.dart
@@ -0,0 +1,53 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ServiceModel {
+  final String name;
+  final String ownername;
+  final String address;
+  final String area;
+  final String city;
+  final String state;
+  final String email;
+  final int contact;
+  final int rent;
+
+  const ServiceModel(
+      {this.name,
+      this.ownername,
+      this.address,
+      this.area,
+      this.city,
+      this.state,
+      this.email,
+      this.contact,
+      this.rent});
+  toJson() {
+    return {
+      "Name": name,
+      "ownername": ownername,
+      "address": address,
+      "area": area,
+      "city": city,
+      "state": state,
+      "email": email,
+      "rent": rent,
+      "contact": contact
+    };
+  }
+
+  factory ServiceModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> documentSnapshot) {
+    final data = documentSnapshot.data();
+    return ServiceModel(
+        name: data["name"] ,
+        ownername: data["ownername"] ,
+        address: data["address"],
+        area:data["area"] ,
+        city:data["city"] ,
+        state: data["state"],
+        rent: data["rent"],
+        contact: data["contact"]
+        );
+  }
+}
