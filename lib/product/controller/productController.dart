0493815c4ff7c0cacc9692967fd53a2b33bce048
parth-scrobits <59013532+Parth-inco@users.noreply.github.com>
2023-02-23 18:50:53 +0530

    Firebase added to ProductView.

--- lib/product/controller/productController.dart
+++ lib/product/controller/productController.dart
@@ -1,13 +1,90 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:student_lobby/services/controller/servicesController.dart';
+
+import '../../home/view/registeredServicesView.dart';
 
 class ProductController extends GetxController {
   TextEditingController nameController = TextEditingController();
   TextEditingController descController = TextEditingController();
   TextEditingController priceController = TextEditingController();
+  ServicesController servicesController = Get.put(ServicesController());
+  String dropDownValue = "Select Store";
+  ServiceType selectedType = ServiceType.STATIONERY;
+  var data = <String>["Select Store"].obs; 
+  var db =FirebaseFirestore.instance;
+  RxString uploadImage = "".obs;
 
-  getProductImage() {
-    final image = ImagePicker.platform.getImage(source: ImageSource.gallery);
+  @override
+  void onInit(){
+    super.onInit();
+    getUserServices(ServiceType.STATIONERY);
+
+  }
+
+  getProductImage() async {
+    final productImage =await ImagePicker.platform.getImage(source: ImageSource.gallery);
+    
+    if(productImage!=null){
+      final ref = FirebaseStorage.instance.ref().child("profileImages/"+productImage.name.toString());
+      await ref.putFile(File(productImage.path));
+      ref.getDownloadURL().then((value) {
+        debugPrint(value);
+        uploadImage.value = value;
+        update();
+      });
+    }
+    
+    update();
+  }
+
+  getUserServices(ServiceType? serviceType)async{
+    
+    if(serviceType == ServiceType.SALON){
+      for (int i=0;i<servicesController.userSalon.length;i++){
+        var tempData = await db.collection("salon").doc(servicesController.userSalon[i]).get();
+        data.add(tempData["name"]); 
+      }
+    }
+    else{
+      for (int i=0;i<servicesController.userStationery.length;i++){
+        var tempData = await db.collection("stationery").doc(servicesController.userStationery[i]).get(); 
+        data.add(tempData["name"]); 
+      }
+    }
+    debugPrint("DATA"+data.toString());
+    update();
+  }
+
+  setDropDownValue(String? newValue){
+    dropDownValue=newValue??"No value";
+    update();
+  }
+
+  addProduct() {
+    if(dropDownValue.isEmpty||dropDownValue=="Select Store"){
+      Fluttertoast.showToast(msg: "Please select a store");
+    }
+    else{
+      Map<String,dynamic> dbData = {
+        "name": nameController.text,
+        "desc": descController.text,
+        "price": priceController.text,
+        "type": selectedType==ServiceType.STATIONERY?"Stationery":"Salon",
+        "store": dropDownValue.toString(),
+        "productImage":uploadImage.value
+      };
+      db.collection("products").add(dbData).then((value) {
+        Fluttertoast.showToast(msg: "ProductAdded");
+        Get.back();
+      });
+    }
+
   }
 }
--- lib/product/view/addProductView.dart
+++ lib/product/view/addProductView.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:student_lobby/home/view/registeredServicesView.dart';
 import 'package:student_lobby/widgets/widget.dart';
 
 import '../controller/productController.dart';
@@ -13,43 +15,101 @@ class AddProductView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: InkWell(
-                onTap: () {
-                  productController.getProductImage();
-                },
-                child: Card(
-                  child: SizedBox(
-                    height: 100,
-                    width: 100,
-                    child: Icon(Icons.add_photo_alternate),
+        child: GetBuilder<ProductController>(
+          init: ProductController(),
+          builder: (controller) {
+            return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      productController.getProductImage();
+                    },
+                    child: Card(
+                      child: SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: productController.uploadImage.value==""?Icon(Icons.add_photo_alternate):CachedNetworkImage(imageUrl: productController.uploadImage.value),
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            CustomTextField(
-              textController: productController.nameController,
-              labelText: "Name",
-              prefixIcon: Icon(Icons.abc_rounded),
-              type: false,
-            ),
-            CustomTextField(
-              labelText: "Description",
-              textController: productController.descController,
-              prefixIcon: Icon(Icons.description),
-              type: false,
-            ),
-            CustomTextField(
-              labelText: "Price",
-              textController: productController.priceController,
-              prefixIcon: Icon(Icons.currency_rupee_sharp),
-            ),
-            // Autocomplete(optionsBuilder: optionsBuilder)
-            regularbtn("Add ", () {})
-          ],
+                CustomTextField(
+                  textController: productController.nameController,
+                  labelText: "Name",
+                  prefixIcon: Icon(Icons.abc_rounded),
+                  type: false,
+                ),
+                CustomTextField(
+                  labelText: "Description",
+                  textController: productController.descController,
+                  prefixIcon: Icon(Icons.description),
+                  type: false,
+                ),
+                CustomTextField(
+                  labelText: "Price",
+                  textController: productController.priceController,
+                  prefixIcon: Icon(Icons.currency_rupee_sharp),
+                ),
+                Column(  
+              children: <Widget>[  
+                ListTile(  
+                  title: const Text('SALON'),  
+                  leading: Radio<ServiceType>(  
+                    value: ServiceType.SALON,  
+                    groupValue: productController.selectedType,  
+                    onChanged: (ServiceType? value) {  
+                       productController.data.clear();
+                       productController.data.add("Select Store");
+                       productController.getUserServices(value);
+                       productController.selectedType=value??ServiceType.SALON;
+                       productController.update();
+                       
+                    },  
+                  ),  
+                ),  
+                ListTile(  
+                  title: const Text('STATIONERY'),  
+                  leading: Radio(  
+                    value: ServiceType.STATIONERY,  
+                    groupValue: productController.selectedType,  
+                    onChanged: (ServiceType? value) {  
+                      productController.data.clear();
+                      productController.data.add("Select Store");
+                      productController.getUserServices(value);
+                      productController.selectedType=value??ServiceType.STATIONERY;
+                      productController.update();
+                    },  
+                  ),  
+                ), 
+              ]), 
+
+                DropdownButton(  
+                  // Initial Value
+                  value: productController.dropDownValue, 
+                  // Down Arrow Icon
+                  icon: const Icon(Icons.keyboard_arrow_down),    
+                  // Array list of items
+                  items: productController.data.map((String items) {
+                    return DropdownMenuItem(
+                      value: items,
+                      child: Text(items),
+                    );
+                  }).toList(),
+                  // After selecting the desired option,it will
+                  // change button value to selected value
+                  onChanged: (String? newValue) => productController.setDropDownValue(newValue),
+                ),
+                // Autocomplete(optionsBuilder: optionsBuilder)
+                regularbtn("Add ", () => productController.addProduct()),
+        
+                 
+        
+        
+              ],
+            );
+          }
         ),
       ),
     );
